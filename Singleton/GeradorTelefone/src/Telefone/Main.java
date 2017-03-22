a3c8bcb17a5f00ad4e23795d21e13d644dffd13b
annavicente <anarubiarvicente@gmail.com>
2017-03-22 13:19:58 -0300

    Estruturacao de acordo com o padrao de projeto

---
+++ Singleton/GeradorTelefone/src/Telefone/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Telefone;
+
+/**
+ *
+ * @author 20142BSI0038
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Pegando numero de telefone: " + GeradorTelefone.INSTANCE.getNumero());
+        System.out.println("Pegando numero de telefone: " + GeradorTelefone.INSTANCE.getNumero());
+        System.out.println("Pegando numero de telefone: " + GeradorTelefone.INSTANCE.getNumero());
+        
+    }
+}
