ce7c06625d2da7c9fb599ddbe509a50725f23dce
Pedagogia EC <Pedagogia EC>
2017-05-31 21:21:32 -0300

    add padrao command

---
+++ CommandPattern/src/command/pattern/Command.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package command.pattern;
+
+/**
+ *
+ * @author Psicologia Judo
+ */
+public interface Command {
+    
+    public void execute();
+}
